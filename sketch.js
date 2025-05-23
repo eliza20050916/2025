0ed71ca126d701e2c9912dd027889c85263d1580
eliza20050916 <413730747@o365.tku.edu.tw>
2025-05-23 10:28:59 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+let facemesh;
+let video;
+let predictions = [];
+const points = [
+  409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291,
+  76, 77, 90, 180, 85, 16, 315, 404, 320, 307, 306, 408, 304, 303, 302, 11, 72, 73, 74, 184
+];
+
+function setup() {
+  createCanvas(400, 400);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+
+  facemesh = ml5.facemesh(video, modelReady);
+  facemesh.on("predict", (results) => {
+    predictions = results;
+  });
+}
+
+function modelReady() {
+  console.log("Facemesh model loaded!");
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+  drawFacemesh();
+}
+
+function drawFacemesh() {
+  if (predictions.length > 0) {
+    const keypoints = predictions[0].scaledMesh;
+
+    stroke(255, 0, 0); // 紅色
+    strokeWeight(15); // 線條粗細
+    noFill();
+
+    beginShape();
+    for (let i = 0; i < points.length; i++) {
+      const index = points[i];
+      const [x, y] = keypoints[index];
+      vertex(x, y);
+    }
+    endShape(CLOSE);
+  }
+}
